d6d1b5faf8eed0ecb00e4b4c62a860bb56404b65
Marc Donaldson <marc@marcdonaldson.com>
2020-10-05 17:38:52 +0200

    Added Defaults extension

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "iOSSDK",
     platforms: [
-        .iOS(.v13)
+        .macOS(.v10_14), .iOS(.v13), .tvOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -20,18 +20,23 @@ let package = Package(
         .package(name:"FontAwesome",url: "https://github.com/thii/FontAwesome.swift", from: "1.0.0"),
         .package(name:"SQLite.swift",url:"https://github.com/stephencelis/SQLite.swift",from: "0.0.0"),
         .package(url: "https://github.com/Juanpe/SkeletonView.git", from: "1.7.0"),
-        .package(url: "https://github.com/exyte/Macaw",from:"0.9.7")
+        .package(url: "https://github.com/exyte/Macaw",from:"0.9.7"),
+        .package(url:"https://github.com/sindresorhus/Defaults",from:"4.1.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+      
         .target(
             name: "iOSSDK",
-            dependencies: ["FontAwesome",
+            dependencies: [ "FontAwesome",
+                            "Defaults",
                            "WaterfallGrid",
                            "Macaw",
                            "SkeletonView",
                            .product(name: "SQLite", package: "SQLite.swift"),]),
+        
+       
         .testTarget(
             name: "iOSSDKTests",
             dependencies: ["iOSSDK"]),
