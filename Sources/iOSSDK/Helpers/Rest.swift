64bf13322334b3f9b6b4dd2a48a9a6cd9075f724
Marc Donaldson <marc@marcdonaldson.com>
2020-10-05 16:08:39 +0200

    Added Rest and ImageLoader

---
+++ Sources/iOSSDK/Helpers/Rest.swift
@@ -0,0 +1,37 @@
+//
+//  File.swift
+//  
+//
+//  Created by Marc Donaldson on 05/10/2020.
+//
+
+import Foundation
+
+class Rest {
+    
+    public static func asyncQuery(address: String,_ action: @escaping (Data?, URLResponse?, Error?) -> Void) {
+        
+        let url = URL(string: address)
+        
+        URLSession.shared.dataTask(with: url!) {(data, response, error) in
+            action(data,response,error)
+        }
+    }
+    
+    public static func syncQuery(address: String) -> Data {
+        let url = URL(string: address)
+        let semaphore = DispatchSemaphore(value: 0)
+        
+        var result: Data = Data()
+        
+        let task = URLSession.shared.dataTask(with: url!) {(data, response, error) in
+            result = data!
+            semaphore.signal()
+        }
+        
+        task.resume()
+        semaphore.wait()
+        return result
+    }
+
+}
