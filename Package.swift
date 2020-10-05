289a21f1e279c477d6cbf076f3331c26118f8b83
Marc Donaldson <marc@marcdonaldson.com>
2020-10-05 13:01:55 +0200

    Added SkeletonView swift package

--- Package.swift
+++ Package.swift
@@ -20,6 +20,7 @@ let package = Package(
         .package(name:"FontAwesome",url: "https://github.com/thii/FontAwesome.swift", from: "1.0.0"),
         .package(name:"SQLite.swift",url:"https://github.com/stephencelis/SQLite.swift",from: "0.0.0"),
         .package(name:"StatusAlert",url:"https://github.com/LowKostKustomz/StatusAlert",from:"0.0.0"),
+        .package(url:"https://github.com/Juanpe/SkeletonView",from:"0.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -28,6 +29,7 @@ let package = Package(
             name: "iOSSDK",
             dependencies: ["FontAwesome",
                            "WaterfallGrid",
+                           "SkeletonView",
                            .product(name: "SQLite", package: "SQLite.swift"),
                            "StatusAlert"]),
         .testTarget(
