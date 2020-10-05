56759b7685982511f5618492591a127f0ba36cb0
Marc Donaldson <marc@marcdonaldson.com>
2020-10-05 16:43:14 +0200

    Fixed Data Loading for Remote URL in Rest

--- Sources/iOSSDK/Helpers/Rest.swift
+++ Sources/iOSSDK/Helpers/Rest.swift
@@ -9,10 +9,12 @@ import Foundation
 
 public class Rest {
     
-    public static func asyncQuery(address: String,_ action: @escaping (Data?, URLResponse?, Error?) -> Void) {
+    public static func asyncQuery(address: String,action: @escaping (Data?, URLResponse?, Error?) -> Void) {
         
         let url = URL(string: address)
-        URLSession.shared.dataTask(with: url!,completionHandler: action)
+        
+        let task =  URLSession.shared.dataTask(with: url!,completionHandler: action)
+        task.resume()
     }
     
     public static func syncQuery(address: String) -> Data {
